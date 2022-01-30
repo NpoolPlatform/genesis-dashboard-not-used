80d8419b4e53d4d1e9bbcf9c07df86da888d3975
EntropyPool <69444208+EntropyPool@users.noreply.github.com>
2022-01-30 22:49:30 +0800

    Update axios.ts
    withCredentials true

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -16,7 +16,7 @@ declare module '@vue/runtime-core' {
 const api = axios.create({
   method: 'POST',
   baseURL: process.env.DEV ? '/api' : 'https://api.internal-devops.development.xpool.top/api',
-  withCredentials: false,
+  withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
   responseType: 'json',
   timeout: 10000
